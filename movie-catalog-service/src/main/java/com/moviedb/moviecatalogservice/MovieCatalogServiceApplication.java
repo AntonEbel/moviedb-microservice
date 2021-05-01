bfa2e73cbae020d3a58370e7c4bcd6b53b1d761b
Anton Ebel <antonebel86@gmail.com>
2021-05-02 01:45:07 +0200

    add jib, kubernetes, dekorateio

--- movie-catalog-service/src/main/java/com/moviedb/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/moviedb/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -1,9 +1,13 @@
 package com.moviedb.moviecatalogservice;
 
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@KubernetesApplication
 public class MovieCatalogServiceApplication {
 
     public static void main(String[] args) {
--- movie-info-service/src/main/java/com/moviedb/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/moviedb/movieinfoservice/MovieInfoServiceApplication.java
@@ -1,9 +1,13 @@
 package com.moviedb.movieinfoservice;
 
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@KubernetesApplication
 public class MovieInfoServiceApplication {
 
     public static void main(String[] args) {
